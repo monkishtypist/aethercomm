890ac97c4e037305b19dec139f689264379b78ea
Monkishtypist <tim@monkishtypist.com>
2019-04-08 21:53:57 +0200

    theme updates

--- functions.php
+++ functions.php
@@ -169,6 +169,116 @@ if ( ! function_exists( 'understrap_add_site_info' ) ) {
 }
 
 /* 7. Custom Post Types */
+add_action( 'init', 'aethercomm_products_post_type' );
+if ( ! function_exists( 'aethercomm_products_post_type' ) ) {
+    function aethercomm_products_post_type() {
+        $labels = array(
+            'name'                  => _x( 'Products', 'aethercomm_products_post_type', 'aethercomm' ),
+            'singular_name'         => _x( 'Product', 'aethercomm_products_post_type', 'aethercomm' ),
+            'menu_name'             => _x( 'Products', 'aethercomm_products_post_type', 'aethercomm' ),
+            'name_admin_bar'        => _x( 'Products', 'aethercomm_products_post_type', 'aethercomm' ),
+            'archives'              => __( 'Item Archives', 'aethercomm' ),
+            'parent_item_colon'     => __( 'Parent Item:', 'aethercomm' ),
+            'all_items'             => __( 'All Products', 'aethercomm' ),
+            'add_new_item'          => __( 'Add New Product', 'aethercomm' ),
+            'add_new'               => __( 'Add New', 'aethercomm' ),
+            'new_item'              => __( 'New Product', 'aethercomm' ),
+            'edit_item'             => __( 'Edit Product', 'aethercomm' ),
+            'update_item'           => __( 'Update Product', 'aethercomm' ),
+            'view_item'             => __( 'View Product', 'aethercomm' ),
+            'search_items'          => __( 'Search Products', 'aethercomm' ),
+            'not_found'             => __( 'Not found', 'aethercomm' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'aethercomm' ),
+            'featured_image'        => __( 'Featured Image', 'aethercomm' ),
+            'set_featured_image'    => __( 'Set featured image', 'aethercomm' ),
+            'remove_featured_image' => __( 'Remove featured image', 'aethercomm' ),
+            'use_featured_image'    => __( 'Use as featured image', 'aethercomm' ),
+            'insert_into_item'      => __( 'Insert into item', 'aethercomm' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this item', 'aethercomm' ),
+            'items_list'            => __( 'Items list', 'aethercomm' ),
+            'items_list_navigation' => __( 'Items list navigation', 'aethercomm' ),
+            'filter_items_list'     => __( 'Filter items list', 'aethercomm' ),
+        );
+        $rewrite = array(
+            'slug'                  => 'product',
+            'with_front'            => false,
+            'pages'                 => true,
+            'feeds'                 => false,
+        );
+        $args = array(
+            'label'                 => _x( 'Products', 'aethercomm_products_post_type', 'aethercomm' ),
+            'description'           => __( 'Company Product Page', 'aethercomm' ),
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
+            'taxonomies'            => array( 'product-categories' ),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'menu_icon'             => 'dashicons-cart',
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => false,
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'rewrite'               => $rewrite,
+            'capability_type'       => 'page',
+        );
+        register_post_type( 'products', $args );
+    }
+}
+
+add_action( 'init', 'aethercomm_product_cats_tax_init' );
+if ( ! function_exists( 'aethercomm_product_cats_tax_init' ) ) {
+    function aethercomm_product_cats_tax_init() {
+
+        $labels = array(
+            'name'                       => _x( 'Product Categories', 'far_productmembers_post_type', $this->textdomain ),
+            'singular_name'              => _x( 'Product Category', 'far_productmembers_post_type', $this->textdomain ),
+            'search_items'               => __( 'Search Product Categories', $this->textdomain ),
+            'popular_items'              => __( 'Popular Product Categories', $this->textdomain ),
+            'all_items'                  => __( 'All Product Categories', $this->textdomain ),
+            'parent_item'                => __( 'Parent Product Category', $this->textdomain ),
+            'parent_item_colon'          => __( 'Parent Product Category:', $this->textdomain ),
+            'edit_item'                  => __( 'Edit Product Category', $this->textdomain ),
+            'view_item'                  => __( 'View Product Category', $this->textdomain ),
+            'update_item'                => __( 'Update Product Category', $this->textdomain ),
+            'add_new_item'               => __( 'Add New Product Category', $this->textdomain ),
+            'new_item_name'              => __( 'New Position', $this->textdomain ),
+            'separate_items_with_commas' => __( 'Separate product categories with commas', $this->textdomain ),
+            'add_or_remove_items'        => __( 'Add or remove product categories', $this->textdomain ),
+            'choose_from_most_used'      => __( 'Choose from the most used product categories', $this->textdomain ),
+            'not_found'                  => __( 'No product categories found', $this->textdomain ),
+            'no_terms'                   => __( 'No product categories', $this->textdomain ),
+            'items_list_navigation'      => __( 'Items list navigation', $this->textdomain ),
+            'items_list'                 => __( 'Items list', $this->textdomain ),
+            'most_used'                  => __( 'Most Used', $this->textdomain ),
+            'back_to_terms'              => __( 'Back to product categories', $this->textdomain ),
+        );
+        $rewrite = array(
+            'slug'                       => 'product-categories',
+            'with_front'                 => false,
+            'hierarchical'               => true
+        );
+        $args = array(
+            'labels'                     => $labels,
+            'description'                => __( 'Product categories', $this->textdomain ),
+            'public'                     => true,
+            'publicly_queryable'         => true,
+            'hierarchical'               => true,
+            'show_ui'                    => true,
+            'show_in_menu'               => true,
+            'show_in_nav_menus'          => true,
+            'show_admin_column'          => true,
+            'rewrite'                    => $rewrite
+        );
+
+        register_taxonomy( 'products', 'product-categories', $args );
+    }
+}
+
 add_action( 'init', 'aethercomm_teammember_post_type' );
 if ( ! function_exists( 'aethercomm_teammember_post_type' ) ) {
     function aethercomm_teammember_post_type() {
@@ -230,9 +340,70 @@ if ( ! function_exists( 'aethercomm_teammember_post_type' ) ) {
     }
 }
 
+add_action( 'init', 'aethercomm_representative_post_type' );
+if ( ! function_exists( 'aethercomm_representative_post_type' ) ) {
+    function aethercomm_representative_post_type() {
+        $labels = array(
+            'name'                  => _x( 'Representatives', 'aethercomm_representatives_post_type', 'aethercomm' ),
+            'singular_name'         => _x( 'Representative', 'aethercomm_representatives_post_type', 'aethercomm' ),
+            'menu_name'             => _x( 'Representatives', 'aethercomm_representatives_post_type', 'aethercomm' ),
+            'name_admin_bar'        => _x( 'Representatives', 'aethercomm_representatives_post_type', 'aethercomm' ),
+            'archives'              => __( 'Item Archives', 'aethercomm' ),
+            'parent_item_colon'     => __( 'Parent Item:', 'aethercomm' ),
+            'all_items'             => __( 'All Representatives', 'aethercomm' ),
+            'add_new_item'          => __( 'Add New Representative', 'aethercomm' ),
+            'add_new'               => __( 'Add New', 'aethercomm' ),
+            'new_item'              => __( 'New Representative', 'aethercomm' ),
+            'edit_item'             => __( 'Edit Representative', 'aethercomm' ),
+            'update_item'           => __( 'Update Representative', 'aethercomm' ),
+            'view_item'             => __( 'View Representative', 'aethercomm' ),
+            'search_items'          => __( 'Search Representatives', 'aethercomm' ),
+            'not_found'             => __( 'Not found', 'aethercomm' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'aethercomm' ),
+            'featured_image'        => __( 'Featured Image', 'aethercomm' ),
+            'set_featured_image'    => __( 'Set featured image', 'aethercomm' ),
+            'remove_featured_image' => __( 'Remove featured image', 'aethercomm' ),
+            'use_featured_image'    => __( 'Use as featured image', 'aethercomm' ),
+            'insert_into_item'      => __( 'Insert into item', 'aethercomm' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this item', 'aethercomm' ),
+            'items_list'            => __( 'Items list', 'aethercomm' ),
+            'items_list_navigation' => __( 'Items list navigation', 'aethercomm' ),
+            'filter_items_list'     => __( 'Filter items list', 'aethercomm' ),
+        );
+        $rewrite = array(
+            'slug'                  => 'representative',
+            'with_front'            => false,
+            'pages'                 => true,
+            'feeds'                 => false,
+        );
+        $args = array(
+            'label'                 => _x( 'Representatives', 'aethercomm_representatives_post_type', 'aethercomm' ),
+            'description'           => __( 'Company Representative Profile Page', 'aethercomm' ),
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
+            'taxonomies'            => array( '' ),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'menu_icon'             => 'dashicons-groups',
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => false,
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'rewrite'               => $rewrite,
+            'capability_type'       => 'page',
+        );
+        register_post_type( 'representatives', $args );
+    }
+}
+
 /* 8. Modify the Query */
-if ( ! function_exists( 'modify_query' ) ) {
-    function modify_query( $query ) {
+if ( ! function_exists( 'aethercomm_modify_query' ) ) {
+    function aethercomm_modify_query( $query ) {
         if ( is_admin() || ! $query->is_main_query() )
             return;
 
