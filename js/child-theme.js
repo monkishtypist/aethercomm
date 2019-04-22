8bc07a2b37cd05bcaa099363e35f6ce3b3942820
Monkishtypist <tim@monkishtypist.com>
2019-04-22 13:46:16 +0100

    theme updates

--- js/child-theme.js
+++ js/child-theme.js
@@ -7053,6 +7053,8 @@
  */
 (function($) {
 
+    // Main Menu: transition background based on scroll position
+
     var wrapperNavbar = $("#wrapper-navbar"); //caches a jQuery object containing the header element
 
     var scrollOffset = 200;
@@ -7069,6 +7071,26 @@
             wrapperNavbar.removeClass("nav-transparency");
         } else {
             wrapperNavbar.addClass("nav-transparency");
+        }
+    });
+
+    // Search Form: open/close input field actions
+
+    var searchform = $("#searchform");
+    var searchinput = $("#s");
+    var searchsubmit = $("#searchsubmit");
+
+    searchsubmit.click(function(){
+        e.preventDefault();
+
+        if (searchform.is(".show") ){
+            if ($.trim(searchinput.val()) != "" ){
+                searchform.submit();
+            } else {
+                searchform.removeClass("show");
+            }
+        } else {
+            searchform.addClass("show");
         }
     });
 
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -5,6 +5,8 @@
  */
 (function($) {
 
+    // Main Menu: transition background based on scroll position
+
     var wrapperNavbar = $("#wrapper-navbar"); //caches a jQuery object containing the header element
 
     var scrollOffset = 200;
@@ -21,6 +23,26 @@
             wrapperNavbar.removeClass("nav-transparency");
         } else {
             wrapperNavbar.addClass("nav-transparency");
+        }
+    });
+
+    // Search Form: open/close input field actions
+
+    var searchform = $("#searchform");
+    var searchinput = $("#s");
+    var searchsubmit = $("#searchsubmit");
+
+    searchsubmit.click(function(){
+        e.preventDefault();
+
+        if (searchform.is(".show") ){
+            if ($.trim(searchinput.val()) != "" ){
+                searchform.submit();
+            } else {
+                searchform.removeClass("show");
+            }
+        } else {
+            searchform.addClass("show");
         }
     });
 
