94369e33aac0a8e8a5e89561ef3819a280698a86
Monkishtypist <tim@monkishtypist.com>
2019-04-25 07:08:27 +0200

    theme updates

--- loop-templates/content-card.php
+++ loop-templates/content-card.php
@@ -11,6 +11,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post;
 
+$card_text = null;
+switch ($post->post_type) {
+    case 'team-member':
+        $card_text = apply_filters( 'the_content', get_acf_field( 'team_member_position', true ) );
+        break;
+    default:
+        $card_text = apply_filters( 'the_excerpt', get_the_excerpt() );
+        break;
+}
+
 ?>
 
 <div class="card" data-post-type="<?php echo $post->post_type; ?>">
@@ -18,13 +28,13 @@ global $post;
     <?php if ( has_post_thumbnail() ) {
         the_post_thumbnail( 'card-img-top', array( 'class' => 'card-img-top' ) );
     } else { ?>
-        <img class="card-img-top" width="245" height="221" src="<?php echo get_stylesheet_directory_uri(); ?>/images/team_default.png" alt="<?php the_title(); ?>" />
+        <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/team_default.png" alt="<?php the_title(); ?>" />
     <?php } ?>
 
     <div class="card-body">
         <div class="card-meta"></div>
         <?php the_title( '<h3 class="card-title">', '</h3>' ); ?>
-        <div class="card-text"><?php the_excerpt(); ?></div>
+        <div class="card-text"><?php ; ?></div>
     </div>
 
 </div>
