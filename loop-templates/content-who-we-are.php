299163e996761f07b6027b3c955ab84564aecf81
Monkishtypist <tim@monkishtypist.com>
2019-04-25 23:40:55 +0200

    theme updates

--- loop-templates/content-who-we-are.php
+++ loop-templates/content-who-we-are.php
@@ -84,11 +84,9 @@ if ( get_acf_field( 'who-we-are_page_settings_mission_background' ) ) {
         </div>
 
         <div class="<?php echo esc_attr( $container ); ?>" id="milestones-tabs-content">
-            <div class="<?php echo esc_attr( $container ); ?>">
-                <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">...tab1</div>
-                <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">...tab2</div>
-                <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">...tab3</div>
-            </div>
+            <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">...tab1</div>
+            <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">...tab2</div>
+            <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">...tab3</div>
         </div>
 
     </section>
