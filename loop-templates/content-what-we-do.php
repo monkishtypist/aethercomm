7d381e2d5df759ecb6fa71e074781dda6fa42635
Monkishtypist <tim@monkishtypist.com>
2019-04-26 12:56:17 +0200

    theme updates

--- loop-templates/content-what-we-do.php
+++ loop-templates/content-what-we-do.php
@@ -13,7 +13,11 @@ global $post;
 
 $container = get_theme_mod( 'understrap_container_type' );
 
-$section_header_styles = '/wp-content/uploads/2019/04/what-we-do-hero.png';
+$section_header_styles = sprintf( 'style="%1$s"',
+    sprintf( 'background-image:url(%1$s);',
+        '/wp-content/uploads/2019/04/what-we-do-hero.png'
+    )
+);
 if ( get_acf_field( 'what-we-do_page_settings_header_background' ) ) {
     $section_header_background_img_id = get_acf_field( 'what-we-do_page_settings_header_background' );
     if ( is_array( $section_header_background_img_id ) && isset( $section_header_background_img_id[0] ) ) {
@@ -103,6 +107,112 @@ if ( get_acf_field( 'what-we-do_page_settings_capabilities_background' ) ) {
 
         <div class="<?php echo esc_attr( $container ); ?>">
 
+            <div class="card-deck">
+
+                <div class="card card-capabilities-card1">
+
+                    <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card1_icon' ) ) {
+                        $capabilities_cards_card1_icon = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card1_icon' );
+                        if ( is_array( $capabilities_cards_card1_icon ) && isset( $capabilities_cards_card1_icon[0] ) ) {
+                            $capabilities_cards_card1_icon_url = wp_get_attachment_url( $capabilities_cards_card1_icon[0] );
+                            echo sprintf( '<img class="card-img-top" src="%1$s" alt="%2$s" />',
+                                $capabilities_cards_card1_icon_url,
+                                $post->$post_title
+                            );
+                        }
+                    } ?>
+
+                    <div class="card-body">
+                        <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card1_label' ) ) {
+                            echo sprintf( '<h3 class="card-title">%1$s</h3>',
+                                get_acf_field( 'what-we-do_page_settings_capabilities_cards_card1_label', true )
+                            ); ?>
+                        <div class="card-text">
+                        <?php
+                        $capabilities_card1_link = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card1_link' );
+                        echo sprintf( '<a href="%1$s" class="btn btn-outline-white" %2$s>%3$s</a>',
+                            esc_url( $capabilities_card1_link[0]['url'] ),
+                            sprintf( 'target="%1$s"',
+                                $capabilities_card1_link[0]['target']
+                            ),
+                            esc_html__( $capabilities_card1_link[0]['title'], 'aethercomm' )
+                        );
+                        ?>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card card-capabilities-card2">
+
+                    <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card2_icon' ) ) {
+                        $capabilities_cards_card2_icon = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card2_icon' );
+                        if ( is_array( $capabilities_cards_card2_icon ) && isset( $capabilities_cards_card2_icon[0] ) ) {
+                            $capabilities_cards_card2_icon_url = wp_get_attachment_url( $capabilities_cards_card2_icon[0] );
+                            echo sprintf( '<img class="card-img-top" src="%1$s" alt="%2$s" />',
+                                $capabilities_cards_card2_icon_url,
+                                $post->$post_title
+                            );
+                        }
+                    } ?>
+
+                    <div class="card-body">
+                        <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card2_label' ) ) {
+                            echo sprintf( '<h3 class="card-title">%1$s</h3>',
+                                get_acf_field( 'what-we-do_page_settings_capabilities_cards_card2_label', true )
+                            ); ?>
+                        <div class="card-text">
+                        <?php
+                        $capabilities_card2_link = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card2_link' );
+                        echo sprintf( '<a href="%1$s" class="btn btn-outline-white" %2$s>%3$s</a>',
+                            esc_url( $capabilities_card2_link[0]['url'] ),
+                            sprintf( 'target="%1$s"',
+                                $capabilities_card2_link[0]['target']
+                            ),
+                            esc_html__( $capabilities_card2_link[0]['title'], 'aethercomm' )
+                        );
+                        ?>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card card-capabilities-card3">
+
+                    <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card3_icon' ) ) {
+                        $capabilities_cards_card3_icon = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card3_icon' );
+                        if ( is_array( $capabilities_cards_card3_icon ) && isset( $capabilities_cards_card3_icon[0] ) ) {
+                            $capabilities_cards_card3_icon_url = wp_get_attachment_url( $capabilities_cards_card3_icon[0] );
+                            echo sprintf( '<img class="card-img-top" src="%1$s" alt="%2$s" />',
+                                $capabilities_cards_card3_icon_url,
+                                $post->$post_title
+                            );
+                        }
+                    } ?>
+
+                    <div class="card-body">
+                        <?php if ( get_acf_field( 'what-we-do_page_settings_capabilities_cards_card3_label' ) ) {
+                            echo sprintf( '<h3 class="card-title">%1$s</h3>',
+                                get_acf_field( 'what-we-do_page_settings_capabilities_cards_card3_label', true )
+                            ); ?>
+                        <div class="card-text">
+                        <?php
+                        $capabilities_card3_link = get_acf_field( 'what-we-do_page_settings_capabilities_cards_card3_link' );
+                        echo sprintf( '<a href="%1$s" class="btn btn-outline-white" %2$s>%3$s</a>',
+                            esc_url( $capabilities_card3_link[0]['url'] ),
+                            sprintf( 'target="%1$s"',
+                                $capabilities_card3_link[0]['target']
+                            ),
+                            esc_html__( $capabilities_card3_link[0]['title'], 'aethercomm' )
+                        );
+                        ?>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
             <div class="row">
                 <div class="col-12 col-lg-6">
                     <?php echo sprintf( '<h2 class="">%1$s</h2>',
@@ -111,49 +221,6 @@ if ( get_acf_field( 'what-we-do_page_settings_capabilities_background' ) ) {
                     <?php echo apply_filters( 'the_content', get_acf_field( 'what-we-do_page_settings_capabilities_copy', true ) ); ?>
                 </div>
             </div>
-
-        </div>
-
-    </section>
-
-    <section id="<?php echo $post->post_name; ?>_team" class="section-white">
-
-        <div class="<?php echo esc_attr( $container ); ?>">
-
-            <div id="team-members-title">
-                <?php echo sprintf( '<h2 class="">%1$s</h2>',
-                    __( 'Team', 'aethercomm' )
-                ); ?>
-            </div>
-
-            <?php
-            $args = array(
-                'post_type' => 'team-members',
-                'posts_per_page' => 20
-            );
-            $query = new WP_Query( $args );
-            ?>
-
-            <?php if ( $query->have_posts() ) : ?>
-
-                <?php ob_start(); ?>
-
-                <div id="team-members-cards" class="card-deck">
-
-                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-
-                        <?php get_template_part( 'loop-templates/content', 'card' ); ?>
-
-                    <?php endwhile; ?>
-
-                </div>
-
-                <?php echo ob_get_clean(); ?>
-
-            <?php endif; wp_reset_query(); ?>
-
-            <span class="crosshairs-blue crosshairs-bottom-left"></span>
-            <span class="crosshairs-blue crosshairs-bottom-right"></span>
 
         </div>
 
