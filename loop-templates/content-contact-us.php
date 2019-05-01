ba7f9eb08a3244bca0192bce681fc954dc0fa795
Monkishtypist <tim@monkishtypist.com>
2019-05-01 18:35:18 +0200

    theme updates

--- loop-templates/content-contact-us.php
+++ loop-templates/content-contact-us.php
@@ -49,52 +49,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
     </section>
 
-    <section id="<?php echo $post->post_name; ?>_benefits" class="section-blue" <?php echo $section_benefits_styles; ?> >
+    <section id="<?php echo $post->post_name; ?>_recommended_hotel" class="section-white">
 
         <div class="<?php echo esc_attr( $container ); ?>">
 
             <div class="row">
+                <?php if ( get_acf_field( 'contact_us_page_settings_recommended_hotel_title' ) ) { ?>
+                    <div class="col-12 col-lg-6"></div>
+                <?php } ?>
                 <div class="col">
                     <?php echo sprintf( '<h2 class="section-title">%1$s</h2>',
-                        get_acf_field( 'contact_us_page_settings_benefits_title', true )
+                        __( 'Recommended Hotel', 'aethercomm' )
                     ); ?>
-                    <?php echo apply_filters( 'the_content', get_acf_field( 'contact_us_page_settings_benefits_copy', true ) ); ?>
+                    <?php echo apply_filters( 'the_content', get_acf_field( 'contact_us_page_settings_recommended_hotel_copy', true ) ); ?>
                 </div>
             </div>
 
         </div>
 
     </section>
-
-    <section id="<?php echo $post->post_name; ?>_open_positions" class="section-white section-banner-wide" <?php echo $section_open_positions_styles; ?> >
-
-        <div class="<?php echo esc_attr( $container ); ?>">
-
-            <div class="row">
-                <div class="col-12">
-                    <?php echo sprintf( '<h2 class="section-title">%1$s</h2>',
-                        get_acf_field( 'contact_us_page_settings_open_positions_title', true )
-                    ); ?>
-                    <?php echo apply_filters( 'the_content', get_acf_field( 'contact_us_page_settings_open_positions_content', true ) ); ?>
-                </div>
-            </div>
-
-        </div>
-
-    </section>
-
-    <?php if ( class_exists( 'RevSlider' ) ) : ?>
-
-        <section id="<?php echo $post->post_name; ?>_slider" class="section-unpadded">
-
-            <div class="container-fluid">
-                <?php putRevSlider("contact_us"); ?>
-            </div>
-
-        </section>
-
-    <?php endif; ?>
-
-    <?php get_template_part( 'sidebar-templates/sidebar', 'contact' ); ?>
 
 </article>
