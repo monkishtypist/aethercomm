3b6abe049bce95333b5e82942ef15598dff2aab7
Monkishtypist <tim@monkishtypist.com>
2019-05-05 15:05:25 +0200

    theme updates

--- loop-templates/content-card.php
+++ loop-templates/content-card.php
@@ -12,12 +12,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $post;
 
 $card_text = null;
+$card_img = false;
 switch ( $post->post_type ) {
     case 'team-members':
         $card_text = apply_filters( 'the_content', get_acf_field( 'team_member_position', true ) );
+        $card_img = true;
+        break;
+    case 'representatives':
+        $card_text = apply_filters( 'the_content', get_the_content() );
+        $card_img = false;
         break;
     default:
         $card_text = apply_filters( 'the_excerpt', get_the_excerpt() );
+        $card_img = true;
         break;
 }
 
@@ -25,7 +32,7 @@ switch ( $post->post_type ) {
 
 <div class="card card-<?php echo $post->post_type; ?>" data-post-type="<?php echo $post->post_type; ?>">
 
-    <?php if ( has_post_thumbnail() ) {
+    <?php if ( has_post_thumbnail() && $card_img ) {
         the_post_thumbnail( 'card-img-top', array( 'class' => 'card-img-top' ) );
     } else { ?>
         <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/team_default.png" alt="<?php the_title(); ?>" />
