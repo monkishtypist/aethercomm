71f7b2feb72feedda8585478f2876a17947be33d
Monkishtypist <tim@monkishtypist.com>
2019-05-06 09:34:29 +0200

    theme updates

--- loop-templates/content-what-we-do.php
+++ loop-templates/content-what-we-do.php
@@ -13,19 +13,6 @@ global $post;
 
 $container = get_theme_mod( 'understrap_container_type' );
 
-$section_header_background_image_url = '/wp-content/uploads/2019/04/what-we-do-hero.png'; // default
-if ( get_acf_field( 'what-we-do_page_settings_header_background' ) ) {
-    $section_header_background_img_id = get_acf_field( 'what-we-do_page_settings_header_background' );
-    if ( is_array( $section_header_background_img_id ) && isset( $section_header_background_img_id[0] ) ) {
-        $section_header_background_image_url = wp_get_attachment_url( $section_header_background_img_id[0] );
-    }
-}
-$section_header_styles = sprintf( 'style="%1$s"',
-    sprintf( 'background-image:url(%1$s);',
-        $section_header_background_image_url
-    )
-);
-
 $section_capabilities_background_image_url = '/wp-content/uploads/2019/04/what-we-do_capabilities-background.png'; // default
 if ( get_acf_field( 'what-we-do_page_settings_capabilities_background' ) ) {
     $section_capabilities_background_img_id = get_acf_field( 'what-we-do_page_settings_capabilities_background' );
@@ -43,28 +30,7 @@ $section_capabilities_styles = sprintf( 'style="%1$s"',
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-    <section id="<?php echo $post->post_name; ?>_header" class="section_header section-black" <?php // echo $section_header_styles; ?> >
-
-        <?php get_template_part( 'global-templates/header', 'image' ); ?>
-
-        <div class="<?php echo esc_attr( $container ); ?>">
-
-            <header>
-                <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
-                <?php if ( get_acf_field( 'what-we-do_page_settings_header_lede' ) ) { ?>
-                    <div class="page-lede"><?php echo apply_filters( 'the_content', get_acf_field( 'what-we-do_page_settings_header_lede', true ) ); ?></div>
-                <?php } ?>
-                <?php if ( get_acf_field( 'what-we-do_page_settings_header_copy' ) ) { ?>
-                    <?php echo apply_filters( 'the_content', get_acf_field( 'what-we-do_page_settings_header_copy', true ) ); ?>
-                <?php } ?>
-            </header>
-
-            <span class="crosshairs-white crosshairs-sm-gray crosshairs-top-left"></span>
-            <span class="crosshairs-white crosshairs-top-right"></span>
-
-        </div>
-
-    </section>
+    <?php get_template_part( 'global-templates/header' ); ?>
 
     <section id="<?php echo $post->post_name; ?>_tabs" class="section-unpadded">
 
