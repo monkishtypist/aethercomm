0b264e435b9c4cea65e1d22119c7418cc0d45a45
Monkishtypist <tim@monkishtypist.com>
2019-05-06 18:02:46 +0200

    theme updates

--- loop-templates/content-latest-post.php
+++ loop-templates/content-latest-post.php
@@ -15,12 +15,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-    <?php the_content(); ?>
-
     <?php if ( has_post_thumbnail() ) { ?>
         <?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
     <?php } ?>
+
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+    <?php the_content(); ?>
 
 </article>
