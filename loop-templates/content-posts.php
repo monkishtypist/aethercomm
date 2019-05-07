337dbc33172529941584a060b61b53970e08733c
Monkishtypist <tim@monkishtypist.com>
2019-05-07 14:39:14 +0200

    theme updates

--- loop-templates/content-posts.php
+++ loop-templates/content-posts.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <article <?php post_class( 'row no-gutters' ); ?> id="post-<?php the_ID(); ?>">
 
     <?php if ( has_post_thumbnail() ) { ?>
-        <div class="post-image-wrapper col order-last"><?php the_post_thumbnail( 'posts-archive', array( 'class' => 'img-fluid' ) ); ?></div>
+        <div class="post-image-wrapper col-12 col-md order-last"><?php the_post_thumbnail( 'posts-archive', array( 'class' => 'img-fluid' ) ); ?></div>
     <?php } ?>
 
     <div class="post-content-wrapper col-12 col-md-4 order-first">
