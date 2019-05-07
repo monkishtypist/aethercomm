8212be560aea5bc4cc43a3a65d8ebdea7b1bd6c8
Monkishtypist <tim@monkishtypist.com>
2019-05-07 15:43:54 +0200

    theme updates

--- functions.php
+++ functions.php
@@ -157,7 +157,7 @@ if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 	// Adds a custom read more link to all excerpts, manually or automatically generated
 	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
 		if ( ! is_admin() ) {
-			$post_excerpt = $post_excerpt . '<a class="btn btn-read-more aethercomm-read-more-link read-more-link" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More', 'aethercomm' ) . '</a>';
+			$post_excerpt = $post_excerpt . '<a class="btn-read-more aethercomm-read-more-link read-more-link" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More', 'aethercomm' ) . '</a>';
 		}
 		return $post_excerpt;
 	}
