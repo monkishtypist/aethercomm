30f9cfaf66f768d8d7ba8a69510f16ded2267e27
Monkishtypist <tim@monkishtypist.com>
2019-05-08 16:20:45 +0200

    products

--- loop-templates/content-product-row.php
+++ loop-templates/content-product-row.php
@@ -12,8 +12,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <tr>
-    <td></td>
-    <td><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?><?php acf_field( 'product_specs_model_number'); ?></td>
+    <td>
+        <a href="#" class="btn btn-secondary">Request Details</a>
+    </td>
+    <td>
+        <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?>
+        <?php acf_field( 'product_specs_model_number'); ?>
+    </td>
     <td><?php acf_field( 'product_specs_frequency_min'); ?></td>
     <td><?php acf_field( 'product_specs_frequency_max'); ?></td>
     <td><?php acf_field( 'product_specs_watts'); ?></td>
