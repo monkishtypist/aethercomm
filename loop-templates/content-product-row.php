e2b07917cf27b8ad4c3dd10f6a65fc9451d71f27
Monkishtypist <tim@monkishtypist.com>
2019-05-08 16:10:40 +0200

    products

---
+++ loop-templates/content-product-row.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package aethercomm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+
+<tr>
+    <td></td>
+    <td><?php acf_field( 'product_specs_model_number'); ?></td>
+    <td><?php acf_field( 'product_specs_frequency_min'); ?></td>
+    <td><?php acf_field( 'product_specs_frequency_max'); ?></td>
+    <td><?php acf_field( 'product_specs_watts'); ?></td>
+    <td><?php the_excerpt(); ?></td>
+</tr>
