2c73a25af303304e42927cd98703a5d02cf1034a
Monkishtypist <tim@monkishtypist.com>
2019-05-08 14:58:21 +0200

    products

---
+++ loop-templates/content-product-categoriess.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package aethercomm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+
+<?php get_template_part( 'loop-templates/content', 'products' ); ?>
