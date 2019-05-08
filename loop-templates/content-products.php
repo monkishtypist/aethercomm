d022b43c868138a1e6383e3d0f22a03ed52a6ae4
Monkishtypist <tim@monkishtypist.com>
2019-05-08 14:12:47 +0200

    products

--- loop-templates/content-products.php
+++ loop-templates/content-products.php
@@ -13,6 +13,14 @@ global $post;
 
 $container = get_theme_mod( 'understrap_container_type' );
 
+$product_cats_args = array(
+    'taxonomy' => 'product-categories',
+    'orderby' => 'name',
+    'order'   => 'ASC'
+);
+
+$product_cats = get_categories($product_cats_args);
+
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -26,6 +34,67 @@ $container = get_theme_mod( 'understrap_container_type' );
             <?php echo sprintf( '<h2 class="section-title">%1$s</h2>',
                 __( 'Search Products', 'aethercomm' )
             ); ?>
+
+        </div>
+
+    </section>
+
+    <?php if ( $product_cats ) : ?>
+
+        <section id="<?php echo $post->post_name; ?>_categories" class="section-black">
+
+            <div class="<?php echo esc_attr( $container ); ?>">
+
+                <nav class="nav">
+                    <?php foreach ( $product_cats as $product_cat ) : ?>
+                        <a class="nav-link" href="<?php echo get_category_link( $product_cat ); ?>"><?php echo $product_cat->name; ?></a>
+                    <?php endforeach; ?>
+                </nav>
+
+            </div>
+
+        </section>
+
+    <?php endif; ?>
+
+    <section id="<?php echo $post->post_name; ?>_loop" class="section-white">
+
+        <div class="<?php echo esc_attr( $container ); ?>">
+
+            <div id="products-section-title">
+                <?php echo sprintf( '<h2 class="">%1$s</h2>',
+                    __( 'Team', 'aethercomm' )
+                ); ?>
+            </div>
+
+            <?php
+            $args = array(
+                'post_type' => 'team-members',
+                'posts_per_page' => 20
+            );
+            $query = new WP_Query( $args );
+            ?>
+
+            <?php if ( $query->have_posts() ) : ?>
+
+                <?php ob_start(); ?>
+
+                <div id="team-members-cards" class="card-deck">
+
+                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+                        <?php get_template_part( 'loop-templates/content', 'card' ); ?>
+
+                    <?php endwhile; ?>
+
+                </div>
+
+                <?php echo ob_get_clean(); ?>
+
+            <?php endif; wp_reset_query(); ?>
+
+            <span class="crosshairs-blue crosshairs-bottom-left"></span>
+            <span class="crosshairs-blue crosshairs-bottom-right"></span>
 
         </div>
 
