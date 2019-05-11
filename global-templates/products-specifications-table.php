4d5480c3da2c54e2d94168e3e9731070e5faf940
Monkishtypist <tim@monkishtypist.com>
2019-05-11 22:36:00 +0700

    products page

--- global-templates/products-specifications-table.php
+++ global-templates/products-specifications-table.php
@@ -11,37 +11,37 @@ $queried_object = get_queried_object();
 
 <table class="table">
     <tbody>
-        <?php if ( get_acf_field( 'product_specs_frequency_min' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_frequency_min', true ) ) { ?>
             <tr>
                 <td><?php _e( 'Low Frequency', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php echo get_acf_field( 'product_specs_frequency_min', true ); ?> <?php _e( 'MHz', 'aethercomm' ); ?></td>
             </tr>
         <?php } ?>
-        <?php if ( get_acf_field( 'product_specs_frequency_max' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_frequency_max', true ) ) { ?>
             <tr>
                 <td><?php _e( 'High Frequency', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php echo get_acf_field( 'product_specs_frequency_max', true ); ?> <?php _e( 'MHz', 'aethercomm' ); ?></td>
             </tr>
         <?php } ?>
-        <?php if ( get_acf_field( 'product_specs_watts' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_watts', true ) ) { ?>
             <tr>
                 <td><?php _e( 'Output Power', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php echo get_acf_field( 'product_specs_watts', true ); ?> <?php _e( 'watts', 'aethercomm' ); ?></td>
             </tr>
         <?php } ?>
-        <?php if ( get_acf_field( 'product_specs_applied_voltage' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_applied_voltage', true ) ) { ?>
             <tr>
                 <td><?php _e( 'Applied Voltage', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php echo get_acf_field( 'product_specs_applied_voltage', true ); ?> <?php _e( 'Vdc', 'aethercomm' ); ?></td>
             </tr>
         <?php } ?>
-        <?php if ( get_acf_field( 'product_specs_short_protection' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_short_protection', true ) ) { ?>
             <tr>
                 <td><?php _e( 'Short Protection', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php _e( 'Yes', 'aethercomm' ); ?></td>
             </tr>
         <?php } ?>
-        <?php if ( get_acf_field( 'product_specs_over_protection' ) ) { ?>
+        <?php if ( get_acf_field( 'product_specs_over_protection', true ) ) { ?>
             <tr>
                 <td><?php _e( 'Over Protection', 'aethercomm' ); ?></td>
                 <td class="text-right"><?php echo get_acf_field( 'product_specs_over_protection', true ); ?> <?php _e( 'MHz', 'aethercomm' ); ?></td>
