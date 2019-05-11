f5ee312ea92619e4cc6a5ef0c551f3aaee36b7a4
Monkishtypist <tim@monkishtypist.com>
2019-05-11 17:34:13 +0700

    products page

--- loop-templates/single-products.php
+++ loop-templates/single-products.php
@@ -75,16 +75,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                     <table class="table">
                         <tbody>
-                            <?php if ( get_acf_field( 'products_specs_frequency_min' ) ) { ?>
+                            <?php if ( get_acf_field( 'product_specs_frequency_min' ) ) { ?>
                                 <tr>
                                     <td><?php _e( 'Low Frequency', 'aethercomm' ); ?></td>
-                                    <td><?php echo get_acf_field( 'products_specs_frequency_min', true ); ?></td>
+                                    <td><?php echo get_acf_field( 'product_specs_frequency_min', true ); ?></td>
                                 </tr>
                             <?php } ?>
-                            <?php if ( get_acf_field( 'products_specs_frequency_max' ) ) { ?>
+                            <?php if ( get_acf_field( 'product_specs_frequency_max' ) ) { ?>
                                 <tr>
                                     <td><?php _e( 'High Frequency', 'aethercomm' ); ?></td>
-                                    <td><?php echo get_acf_field( 'products_specs_frequency_max', true ); ?></td>
+                                    <td><?php echo get_acf_field( 'product_specs_frequency_max', true ); ?></td>
                                 </tr>
                             <?php } ?>
                         </tbody>
