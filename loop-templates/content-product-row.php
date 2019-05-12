1bc2e1e4f90fbdd524b33c719740b8beeb777374
Monkishtypist <tim@monkishtypist.com>
2019-05-12 22:09:37 +0700

    products page

--- loop-templates/content-product-row.php
+++ loop-templates/content-product-row.php
@@ -14,14 +14,16 @@ $terms_array = array();
 foreach ( $terms as $term ) {
     $terms_array[] = $term->slug;
 }
+
+$model_number = get_acf_field( 'product_specs_model_number', true );
 ?>
 
 <tr>
     <td>
-        <a href="#" class="btn btn-secondary btn-sm"><?php echo sprintf( '%1$s<br />%2$s', __( 'Request', 'aethercomm' ), __( 'Details', 'aethercomm' ) ); ?></a>
-        <a href="#" class="product-queue-link"><?php _e( 'Add to queue', 'aethercomm' ); ?></a>
+        <a href="#" class="product-request btn btn-secondary btn-sm" data-model-number="<?php echo $model_number; ?>"><?php echo sprintf( '%1$s<br />%2$s', __( 'Request', 'aethercomm' ), __( 'Details', 'aethercomm' ) ); ?></a>
+        <a href="#" class="product-queue-link" data-model-number="<?php echo $model_number; ?>"><?php _e( 'Add to queue', 'aethercomm' ); ?></a>
     </td>
-    <td data-order="<?php echo get_acf_field( 'product_specs_model_number', true ); ?>">
+    <td data-order="<?php echo $model_number; ?>">
         <div class="row no-gutters">
             <?php if ( has_post_thumbnail() ) { ?>
                 <div class="col-12 col-md-3 pr-md-1">
@@ -31,7 +33,7 @@ foreach ( $terms as $term ) {
             <div class="col">
                 <?php echo sprintf( '<a href="%1$s" class="btn-link product-link">%2$s</a>',
                     get_permalink(),
-                    get_acf_field( 'product_specs_model_number', true )
+                    $model_number
                 ); ?>
                 <?php echo sprintf( '<a href="%1$s" class="btn-link pdf-link" target="_blank">%2$s</a>',
                     wp_get_attachment_url( get_acf_field( 'product_details_data_sheet', true ) ),
