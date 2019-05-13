92702498906a307edfd8664c19d7f5bc0e5153c2
Monkishtypist <tim@monkishtypist.com>
2019-05-13 18:36:49 +0700

    timeline

--- loop-templates/content-products.php
+++ loop-templates/content-products.php
@@ -158,19 +158,3 @@ $product_cats = get_categories( $product_cats_args );
     <?php get_template_part( 'sidebar-templates/sidebar', 'contact' ); ?>
 
 </article>
-
-<div aria-live="polite" aria-atomic="true" style="position: fixed; min-height: 200px; min-width: 600px;">
-  <div class="toast" style="position: absolute; bottom: 1rem; right: 1rem;">
-    <div class="toast-header">
-      <img src="..." class="rounded mr-2" alt="...">
-      <strong class="mr-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-  </div>
-</div>
