0ac6a4f87e59d234a866ecf05454054e57768124
Monkishtypist <tim@monkishtypist.com>
2019-05-13 19:44:17 +0700

    timeline

--- loop-templates/content-what-we-do.php
+++ loop-templates/content-what-we-do.php
@@ -11,8 +11,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post;
 
+$template_slug = $post->post_name;
+
 $container = get_theme_mod( 'understrap_container_type' );
 
+// Header image overlay
+if ( has_acf_field( 'page_header_background' ) ) {
+    $section_header_image_id = get_acf_field( 'page_header_background', true );
+    $section_header_image = wp_get_attachment_image( $section_header_image_id, 'full', false, array( 'class' => 'section-overlay-image img-fluid' ) );
+} elseif ( has_post_thumbnail() ) {
+    $section_header_image = get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'section-overlay-image img-fluid' ) );
+} else {
+    $section_header_image = false;
+}
+$overlay_color = ( has_acf_field( 'page_header_overlay' ) ? get_acf_field( 'page_header_overlay', true ) : 'black' );
+
+// Page Title & Lede
+$page_title = false;
+$page_lede = false;
+
+if ( has_acf_field( 'page_header_lede' ) ) {
+    $page_title = sprintf( '<h1 class="page-title" data-if="4-1">%1$s</h1>',
+        get_the_title() );
+    $page_lede = sprintf( '<div class="page-lede">%1$s</div>',
+        apply_filters( 'the_content', get_acf_field( 'page_header_lede', true ) ) );
+} else {
+    $page_lede = sprintf( '<h1 class="page-lede" data-if="4-2">%1$s</h1>',
+        get_the_title() );
+}
+
+// Page header Copy
+$page_header_copy = false;
+if ( get_acf_field( 'page_header_copy' ) ) :
+    $page_header_copy = apply_filters( 'the_content', get_acf_field( 'page_header_copy', true ) );
+endif;
+
+$section_header_classes = sprintf( 'section_header %1$s_header ',
+    $template_slug );
+$section_crosshairs = '<span class="crosshairs-gray crosshairs-sm-gray crosshairs-top-left"></span><span class="crosshairs-gray crosshairs-top-right"></span>';
+
+if ( $section_header_image ) {
+    $section_header_classes .= sprintf( 'section-header-overlay section-header-overlay_',
+        $overlay_color );
+    $section_crosshairs = '<span class="crosshairs-white crosshairs-sm-gray crosshairs-top-left"></span><span class="crosshairs-white crosshairs-top-right"></span>';
+}
+
+// section capabilities
 $section_capabilities_background_image_url = '/wp-content/uploads/2019/04/what-we-do_capabilities-background.png'; // default
 if ( get_acf_field( 'what-we-do_page_settings_capabilities_background' ) ) {
     $section_capabilities_background_img_id = get_acf_field( 'what-we-do_page_settings_capabilities_background' );
@@ -30,7 +74,30 @@ $section_capabilities_styles = sprintf( 'style="%1$s"',
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-    <?php get_template_part( 'global-templates/header' ); ?>
+    <section id="<?php echo $post->post_name; ?>_header" class="<?php echo $section_header_classes; ?>">
+
+        <?php if ( $section_header_image ) : ?>
+            <div class="section-image-overlay-wrapper" <?php // echo $section_header_styles; ?>>
+                <div class="section-image-overlay-wrapper-inner">
+                    <div class="overlay"></div>
+                    <?php echo $section_header_image; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <div class="<?php echo esc_attr( $container ); ?>">
+
+            <header>
+                <?php echo $page_title; ?>
+                <?php echo $page_lede; ?>
+                <?php echo $page_header_copy; ?>
+            </header>
+
+            <?php echo $section_crosshairs; ?>
+
+        </div>
+
+    </section>
 
     <section id="<?php echo $post->post_name; ?>_tabs" class="section-unpadded">
 
