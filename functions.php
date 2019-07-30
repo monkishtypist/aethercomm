243670f41cd3988c794db94c5fe042b6a261c37d
Monkishtypist <tim@monkishtypist.com>
2019-07-30 13:58:43 -0700

    remove emoji's from wp 4.2

--- functions.php
+++ functions.php
@@ -75,6 +75,10 @@ if ( ! function_exists( 'aethercomm_wp_loaded' ) ) {
     }
 }
 
+// REMOVE EMOJI ICONS
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
 /**
  * Custom Button
  *
